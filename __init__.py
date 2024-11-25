aa6c404a35696e3e5b61d84ad67d5408c119f78c
SXDIST <nightmoon.yt228@gmail.com>
2024-11-26 00:18:11 +0300

    Added Addon Information and other

--- __init__.py
+++ __init__.py
@@ -1,4 +1,17 @@
-# def register():
+bl_info = {
+    "name" : "DayZ LODs Generation",
+    "description" : "A set of usefull extensions for Arma 3 Object Builder addon | For RKIS Lab",
+    "author" : "SXDIST, Do_Nat",
+    "blender" : (4, 2, 0),
+    "category" : "3D View"
+}
 
+from . import auto_load
 
-# def unregister():
+auto_load.init()
+
+def register():
+    auto_load.register()
+
+def unregister():
+    auto_load.unregister()
